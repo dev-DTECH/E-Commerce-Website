d39e340ed736e4b78e0ebb34d2e1bce6b70a84ee
dev-dtech <deep64222@gmail.com>
2020-03-21 11:10:22 +0530

    restructing folder

--- public/index.js
+++
@@ -1,9 +0,0 @@
-
-// const myfirstelement = <h1>Hello React!</h1>
-
-// ReactDOM.render(myfirstelement, document.getElementById('root'));
-class Hello extends React.Component {
-    render() {
-      return <h1>Hello World!</h1>
-    }
-  }
